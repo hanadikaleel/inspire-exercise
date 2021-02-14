deb995fa08cddef5e28fdf06bc911b4fe1d5d3a3
hanadikaleel <hanadiii>
2021-02-14 12:22:36 -0400

    Added Back to Search on ItemView page, styled ItemView to match mockup design

--- src/Components/ItemView.tsx
+++ src/Components/ItemView.tsx
@@ -1,8 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 export default function ItemView(props:any) {
   let record = props.location.state.record.metadata;
   return (
     <div className="Page">
       <p className="back">
+      <Link to={"/"} style={{ textDecoration: "none" }}> <p> {"< Back to Search Results"} </p>{" "} </Link>
       <div className="list">
           <p className="record-title">{record.titles[0].title} </p>
           <p className="abstract">
